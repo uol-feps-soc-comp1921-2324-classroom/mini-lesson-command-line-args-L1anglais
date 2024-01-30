e53bbbd3125bf4a255f3983235a7a3c78abe7753
Matteo A. Booth <146246888+L1anglais@users.noreply.github.com>
2024-01-30 14:21:59 +0000

    Completed task

--- tasks/task.c
+++ tasks/task.c
@@ -2,12 +2,20 @@
 
 int main(int argc, char *argv[]) {
 
+    // check 3 command line inputs
     if (argc != 3)
     {
         printf("Usage: %s <number 1> <number 2>\n", argv[0]);
         return 0;
     }
     
+    // check numbers entered are numbers
+    if (atoi(argv[1]) < 1 || atoi(argv[2]) < 1)
+    {
+        printf("Error: Invalid numbers entered\n");
+        return 1;
+    }
+
     // Calculate and print the sum
     int sum = atoi(argv[1]) + atoi(argv[2]);
     printf("Sum: %d\n", sum);
